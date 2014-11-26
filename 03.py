37fbcaca585dd3efec7f4617f7db7fdfc2214248
akturtle <258859059@qq.com>
2014-11-26 15:25:15 +0800

    05 Smallest multiple
    To find max power number of  prime under 20

--- 03.py
+++ 03.py
@@ -2,7 +2,7 @@ import math as mt
 
 def isPrime(x):
     flag=1
-    for i in range(2,int(mt.sqrt(x))):
+    for i in range(2,int(mt.sqrt(x))+1):
         if x%i==0:
             flag=0
             break
---
+++ 05.py
@@ -0,0 +1,25 @@
+import math as mt
+
+def isPrime(x):
+    flag=1
+    for i in range(2,int(mt.sqrt(x))+1):
+        if x%i==0:
+            flag=0
+            break
+    return flag
+
+x=21
+list=[]
+for i in range(2,x):
+    if isPrime(i):
+        k=i
+        n=2
+        while  i**n<x:
+            k=i**n
+            n=n+1
+        list.append(k)
+        print(list)
+product=1
+for element in list:
+   product=product*element
+print(product)
